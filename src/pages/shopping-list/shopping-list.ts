2d2137fc53e52a05a55dc3b94e20413921ffcf28
Skyler Norgaard <skylernorgaard@gmail.com>
2017-10-18 23:17:21 -0400

    Last commit of the night
    sdfsdfsdf

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, ModalController, NavParams } from 'ionic-angular';
+import {AlertController} from 'ionic-angular';
+import { IonicPage, NavController, ModalController, NavParams, Events } from 'ionic-angular';
 
 import { ShoppingItem } from '../../models/shopping-item.interface';
 
@@ -23,19 +24,40 @@ import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 })
 export class ShoppingListPage {
 
+  //public ionColor: string = 'primary';
   itemsRef: AngularFireList<any>
   items: Observable<ShoppingItem[]>
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public modalCtrl: ModalController,
-              private db: AngularFireDatabase) {
+              private db: AngularFireDatabase,
+              public events: Events,
+              public alertCtrl: AlertController) {
 
     this.itemsRef = db.list('shopping-list');
     this.items = this.itemsRef.snapshotChanges().map(changes => {
       return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
     });
   }
+
+
+  showAlert() {
+      let alert = this.alertCtrl.create({
+        title: 'Hi',
+        subTitle: 'Something isnt working so we put an alert here',
+        buttons: ['OK :(']
+      });
+      alert.present();
+      this.buttonColor = "primary";
+    //   if(this.ionColor == "primary")
+    //   {
+    //   this.ionColor = "secondary";
+    // }else
+    // {
+    //   this.ionColor = "primary";
+    // }
+    }
 
 
   clickFab() {
