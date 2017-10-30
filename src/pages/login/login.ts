dd6421d28acfcd87bbed27082552dbb84714efdb
Tanush Samson <tanflare@gmail.com>
2017-10-30 17:58:55 -0400

    Login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,AlertController } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
 import { RegisterPage } from '../register/register';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
 
 @IonicPage()
 @Component({
@@ -14,7 +15,10 @@ export class LoginPage {
   email: string;
   password: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public afAuth: AngularFireAuth) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public afAuth: AngularFireAuth,
+              public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
@@ -25,11 +29,27 @@ export class LoginPage {
     this.navCtrl.setRoot(RegisterPage);
   }
 
+  showAlert() {
+      let alert = this.alertCtrl.create({
+        title: 'Error',
+        subTitle: 'Please enter a valid username and password',
+        buttons: ['Ok']
+      });
+      alert.present();
+
+    }
+
   signIn()
   {
-    this.afAuth.auth.signInWithEmailAndPassword(this.email, this.password);
-    this.navCtrl.setRoot(TabsPage);
-  }
+    //this.showAlert(this.email,this.password);
+    if(this.email != undefined && this.password != undefined)
+    {
+      var that = this;
+    this.afAuth.auth.signInWithEmailAndPassword(this.email, this.password).then(function(){that.navCtrl.setRoot(TabsPage);},function(){that.showAlert();});
 
+    } else {
+    this.showAlert();
+    }
+  }
 
 }
