37748defe66a08dbecf978667cf48f186235298e
Jacob Naranjo <jaconaranj57@gmail.com>
2017-11-15 15:04:00 -0500

    some commenting

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,41 +1,30 @@
 import { Component } from '@angular/core';
-import { AlertController, IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController, NavController } from 'ionic-angular';
 import { AngularFireDatabase } from 'angularfire2/database';
-import { AngularFireAuth } from 'angularfire2/auth';
 import { DatabaseProvider } from '../../providers/database/database';
 import { TabsPage } from '../tabs/tabs';
 import { SetupPage } from '../setup/setup';
 import { User } from '../../models/user.interface';
 
 /*
- * Generated class for the RegisterPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
+ * This page allows a user to register an account to HouseHub.
  */
 
-@IonicPage()
 @Component({
   selector: 'page-register',
   templateUrl: 'register.html',
 })
 export class RegisterPage {
 
-  user = {} as User;
-  //userId: string;
+  user = {} as User;  // user object
 
   constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              //public afAuth: AngularFireAuth,
               public alertCtrl: AlertController,
               private db: AngularFireDatabase,
               private dbProv: DatabaseProvider) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterPage');
-  }
-
+  // create account, log in, and navigate to setup page
   async createAccountAndLogIn(email: string, password: string) {
     try {
       await this.dbProv.emailSignUp(email, password, this.user);
