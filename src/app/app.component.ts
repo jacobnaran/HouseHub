9fa9ceaa1099874d2e32198fe795f4726f9e1104
Tanush Samson <tanflare@gmail.com>
2017-11-26 17:22:24 -0500

    Revert "Broken"
    This reverts commit d642b9dc5b6f271373018f8b03cc77dece7d7303.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { App, Events, Platform, AlertController } from 'ionic-angular';
+import { App, Events, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AuthProvider } from '../providers/database/database';
@@ -7,7 +7,6 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from '../pages/login/login';
 import { LoadingPage } from '../pages/loading/loading';
 import { ChatPage } from '../pages/chat/chat';
-import { Push, PushObject, PushOptions} from '@ionic-native/push';
 
 @Component({
   templateUrl: 'app.html'
@@ -20,9 +19,7 @@ export class MyApp {
               splashScreen: SplashScreen,
               public events: Events,
               private app: App,
-              private authProv: AuthProvider,
-              public push: Push,
-            public alertCtrl:AlertController) {
+              private authProv: AuthProvider) {
 
     // listen for logout event then sets login page as root
     this.events.subscribe('user:logout', () => {
@@ -37,40 +34,7 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      this.pushsetup();
       splashScreen.hide();
     });
-  }
-
-  pushsetup() {
-    const options: any = {
-     android: {
-         senderID: "350833857485"
-     },
-     ios: {
-         alert: 'true',
-         badge: true,
-         sound: 'false'
-     },
-     windows: {}
-  };
-
-  const pushObject: PushObject = this.push.init(options);
-
-  pushObject.on('notification').subscribe((notification: any) => {
-    if (notification.additionalData.foreground) {
-      let youralert = this.alertCtrl.create({
-        title: 'New Push notification',
-        message: notification.message
-      });
-      youralert.present();
-    }
-  });
-
-  pushObject.on('registration').subscribe((registration: any) => {
-     //do whatever you want with the registration ID
-  });
-
-  pushObject.on('error').subscribe(error => alert('Error with Push plugin' + error));
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { LocalNotifications } from '@ionic-native/local-notifications';
-import { Push } from '@ionic-native/push';
 
 // firebase stuff
 import { AngularFireModule } from 'angularfire2';
@@ -91,7 +90,6 @@ import { EditReminderComponent } from '../components/edit-reminder/edit-reminder
   providers: [
     StatusBar,
     SplashScreen,
-    Push,
     LocalNotifications,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     AuthProvider
